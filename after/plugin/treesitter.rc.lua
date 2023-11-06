823d4180c6163bb58d7773d8be0bd74e000cca00
git-nasu <nasu301130@icloud.com>
2023-11-06 22:07:08 +0900

    treesitter incremental_selection added

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -4,6 +4,7 @@ if not status then
 end
 
 ts.setup({
+	-- ensure_installed = "python",
 	highlight = {
 		additional_vim_regex_highlighting = true,
 		enable = true,
@@ -20,5 +21,14 @@ ts.setup({
       'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
       'css', 'lua', 'xml', 'php', 'markdown','rb','ejs'
     }
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_selection = "grn",
+			scope_selection = "grc",
+			node_decremental = "grm"
+		}
 	}
 })
